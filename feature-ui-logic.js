93e5339eacd5885ef4d45cb87b5760f087be90db
Sebastian Espinoza <sespinozar25@ilb.cat>
2026-03-18 12:58:25 +0100

    Llenar los selects con las opciones del 0 al 9 y adición de comentarios

--- feature-ui-logic.js
+++ feature-ui-logic.js
@@ -1,3 +1,4 @@
+// Creació de les variables globals 
 const selects = document.querySelectorAll(".code-input");
 const btnEnviar = document.getElementById("btn-enviar");
 const terminal = document.getElementById("terminal");
@@ -6,3 +7,16 @@ const rondesSpan = document.getElementById("rondes-restants");
 let rondes = 5;
 
 const codiSecret = [1, 2, 3, 4];
+
+// Omplir els selects amb les opcions del 0 al 9
+
+function inicialitzarSelects() {
+    selects.forEach(select => {
+        for (let i = 0; i <= 9; i++) {
+            const option = document.createElement("option");
+            option.value = i;
+            option.textContent = i;
+            select.appendChild(option);
+        }
+    });
+}
