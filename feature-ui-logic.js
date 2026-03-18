66d4fdaa48038ccb19d93168654c8e5c8f69ef7e
Sebastian Espinoza <sespinozar25@ilb.cat>
2026-03-18 12:48:58 +0100

    Definir variables globales y codigo secreto que luego se puede generar automaticamente

---
+++ feature-ui-logic.js
@@ -0,0 +1,8 @@
+const selects = document.querySelectorAll(".code-input");
+const btnEnviar = document.getElementById("btn-enviar");
+const terminal = document.getElementById("terminal");
+const rondesSpan = document.getElementById("rondes-restants");
+
+let rondes = 5;
+
+const codiSecret = [1, 2, 3, 4];
