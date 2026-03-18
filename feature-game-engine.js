0603fa043429a079392117ceaa2dc2159599603a
Xavier Pucciarelli <xpucciarelli25@ilb.cat>
2026-03-18 13:25:28 +0100

    Creació d'una funció que comprova el codi introduit per l'usuari i el codi generat.

--- feature-game-engine.js
+++ feature-game-engine.js
@@ -10,3 +10,34 @@ function generarCodiSecret() {
     }
     return codi;
 }
+
+//Comparar el codi intorduit per l'usuari i el codi generat 
+function compararCodis(codiUsuari, codiSecret) {
+
+    // Array buit on guardarem el resultat de cada posició. Es mostrara 1 si es correcte, O si esta en posicio incorrecta, x si no apareix
+    //Aquest es el resultat que es mostrarpa
+    const resultat = [];
+
+    // Recorrem les 4 posicions del codi intorduir per l'usuari
+    for (let i = 0; i < 4; i++) {
+        // Comprovem si el número de l'usuari és igual al del secret i si esta a la mateixa posicio
+        if (codiUsuari[i] === codiSecret[i]) {
+            //Com es el mateix numero i mateixa posició, es posara 1 en l'array de resultat.
+            resultat.push("1");
+
+       
+        // Si NO és igual a la mateixa posició, comprovem si aquest número existeix en alguna altra posició del codi secret
+        } else if (codiSecret.includes(codiUsuari[i])) {
+
+            // Si el número existeix però no està al lloc correcte es posara un O en l'array de resultat
+            resultat.push("Ø");
+
+       //Si el numero no està en posicio correcta ni en cap altre posició, es posara un x en l'array
+        } else {
+            resultat.push("×");
+        }
+    }
+
+    // Per a que el resultat estigui més presentable en comptes d'un array es mostraran els 4 caracters separats per un espai
+    return resultat.join(" ");
+}
